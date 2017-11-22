380ac3cb22b4fa9e5e670603940645b48c9c28dc
unknown <ya13@my.fsu.edu>
2017-11-22 17:35:35 -0500

    Camera now zoomed in on map, moves with arrow keys

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public float MOVEMENT_SPEED = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		// Will need to implement a find player function so camera focuses on player character
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// If arrow keys detected scroll through map
+        //Gets keys for continuous movement
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, MOVEMENT_SPEED, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(MOVEMENT_SPEED, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-MOVEMENT_SPEED, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -MOVEMENT_SPEED, 0));
+        }
+
+        //Will need to implement map limit detector to prevent scrolling infinitely off map, or bind find player function to key
+	}
+}
